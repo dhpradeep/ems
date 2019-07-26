2ab833baeac7250e036935371acf27890b77cef0
dhpradeep <dhpradeep25@gmail.com>
2019-07-26 14:32:47 +0545

    Added database

---
+++ app/sql/ems.sql
@@ -0,0 +1,379 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 26, 2019 at 10:47 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ems`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `connection`
+--
+
+CREATE TABLE `connection` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `resourceId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactdetails`
+--
+
+CREATE TABLE `contactdetails` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `municipality` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `wardNo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `area` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `district` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `zone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `mobileNo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `telephoneNo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `blockNo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `guardianName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `guardianRelation` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `guardianContact` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `documents`
+--
+
+CREATE TABLE `documents` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `formNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `entranceNo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `eligible` tinyint(1) NOT NULL,
+  `minimumPercent` tinyint(1) NOT NULL,
+  `marksheet` tinyint(1) NOT NULL,
+  `characterCertificate` tinyint(1) NOT NULL,
+  `citizenship` tinyint(1) NOT NULL,
+  `photo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `education`
+--
+
+CREATE TABLE `education` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `level` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `institution` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `board` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `yearOfCompletion` int(11) NOT NULL,
+  `percent` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personaldata`
+--
+
+CREATE TABLE `personaldata` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `program` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `doa` datetime NOT NULL,
+  `dobAd` datetime NOT NULL,
+  `dobBs` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `gender` tinyint(11) NOT NULL,
+  `nationality` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Nepali',
+  `fatherName` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `program`
+--
+
+CREATE TABLE `program` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `duration` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questionmodel`
+--
+
+CREATE TABLE `questionmodel` (
+  `id` int(11) NOT NULL,
+  `programId` int(11) NOT NULL,
+  `categoryId` int(11) NOT NULL,
+  `minLevel` int(11) NOT NULL,
+  `maxLevel` int(11) NOT NULL,
+  `noOfQuestions` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL,
+  `categoryId` int(11) NOT NULL,
+  `question` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `level` tinyint(4) NOT NULL,
+  `choice1` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `choice2` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `choice3` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `choice4` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `answer` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `record`
+--
+
+CREATE TABLE `record` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `categoryId` int(11) NOT NULL,
+  `questionId` int(11) NOT NULL,
+  `choice` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `rightAnswer` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `result` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timetrack`
+--
+
+CREATE TABLE `timetrack` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `remainingTime` int(11) NOT NULL,
+  `isSubmitted` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userlogin`
+--
+
+CREATE TABLE `userlogin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `fname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `mname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `passwordHash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `role` tinyint(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `userlogin`
+--
+
+INSERT INTO `userlogin` (`id`, `username`, `fname`, `mname`, `lname`, `email`, `passwordHash`, `role`) VALUES
+(1, 'admin', '', NULL, '', 'admin@admin.com', 'admin', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `connection`
+--
+ALTER TABLE `connection`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contactdetails`
+--
+ALTER TABLE `contactdetails`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `documents`
+--
+ALTER TABLE `documents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `education`
+--
+ALTER TABLE `education`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `personaldata`
+--
+ALTER TABLE `personaldata`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `program`
+--
+ALTER TABLE `program`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questionmodel`
+--
+ALTER TABLE `questionmodel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `record`
+--
+ALTER TABLE `record`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `timetrack`
+--
+ALTER TABLE `timetrack`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userlogin`
+--
+ALTER TABLE `userlogin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `connection`
+--
+ALTER TABLE `connection`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `contactdetails`
+--
+ALTER TABLE `contactdetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `documents`
+--
+ALTER TABLE `documents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `education`
+--
+ALTER TABLE `education`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `personaldata`
+--
+ALTER TABLE `personaldata`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `program`
+--
+ALTER TABLE `program`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `questionmodel`
+--
+ALTER TABLE `questionmodel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `record`
+--
+ALTER TABLE `record`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `timetrack`
+--
+ALTER TABLE `timetrack`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `userlogin`
+--
+ALTER TABLE `userlogin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
